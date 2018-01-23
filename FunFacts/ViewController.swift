94807b20dcf01e0ff30b4ad35a29c43230a66bc8
Kirill <k.koleno@icloud.com>
2018-01-23 13:46:11 +0400

    first commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Kirill Koleno on 25.01.17.
+//  Copyright © 2017 Kirill Koleno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factProvider = FactProvider()
+    let colorProvider = BackgroungColorProvider()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        funFactLabel.text = factProvider.randomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showFact() {
+        funFactLabel.text = factProvider.randomFact()
+        
+        let randomColor = colorProvider.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+    }
+}
